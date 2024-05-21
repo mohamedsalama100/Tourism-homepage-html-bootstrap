90c86ebec046f2f32205fb555276f7479f0f9722
mohamedsalama100 <mohamed.salamaa100a@gmail.com>
2024-05-21 20:59:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// var btn =document.querySelectorAll(".btn3")
+function code1(){
+
+    alert("is copied")
+}
+
+  // btn.onclick=code1
+
+  // let x =14
+  // function code1(){
+  //   if (false){
+  //     let x=28
+  //   }
+  //   console.log(x)
+  // }
